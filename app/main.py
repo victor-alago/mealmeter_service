6cb64c10e073516b40dd0830697c048645f41eff
Alago Victor <123035877+victor-alago@users.noreply.github.com>
2025-01-26 14:05:54 +0100

    Completed the food-log feature

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import auth, food_logging, profile
+from app.routers import auth, profile, insights, food_logging
 
 app = FastAPI()
 
@@ -16,4 +16,5 @@ app.add_middleware(
 # Include the authentication router
 app.include_router(auth.router)
 app.include_router(profile.router)
+app.include_router(insights.router)
 app.include_router(food_logging.router)
